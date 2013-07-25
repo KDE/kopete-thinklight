702dd276ec8201d42a83c2bd5e7d25e0fa1fd13f
Burkhard Lück <lueck@hube-lueck.de>
2013-07-25 07:56:52 +0200

    rename catalog to get translation properly loaded

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cpp` rc.cpp -o $podir/kopete-thinklight.pot
+$XGETTEXT `find . -name \*.cpp` rc.cpp -o $podir/kcm_kopete_thinklight.pot
 rm -f rc.cpp
